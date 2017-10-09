1eed99bbd8aeeac14e57aecb1bda4cee5e1a4afb
marta.adell.noguera <marta.adell.noguera@gmail.com>
2017-10-09 15:12:12 +0200

    crear projecte i poasr la llista al layout

---
+++ app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
@@ -0,0 +1,20 @@
+package edu.upc.eseiaat.pma.countrylist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CountryListActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_list);
+
+
+    }
+}
